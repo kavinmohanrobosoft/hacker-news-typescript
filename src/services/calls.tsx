debe89c264956d821e633911f33e985cc592a899
kavinRobosoft <kavin.s@robosoftin.com>
2020-07-09 17:43:38 +0530

    cu

--- src/services/calls.tsx
+++ src/services/calls.tsx
@@ -1,18 +1,10 @@
 import * as Api from "./apis";
 import { PostsResult } from "../utils/interfaces";
 import axios from "axios";
-// export const getItems = async (itemid:number) => {
-//   const result = await axios
-//   .get(`${Api.baseURL}item/${itemid}.json`)
-//   .then(({ data }) => data && data)
-//   return result;
-// }
+
 export const getData = async (api: string) => {
   const url: string = api;
 
-//   const getIds = await axios.get(url).then(({ data }) => data);
-//   return getIds;
-// };
   try {
     const response = await fetch(url);
     if (response.ok === false) {
@@ -38,9 +30,6 @@ export const getData = async (api: string) => {
 export const getComments = async (api: string) => {
   const url: string = api;
 
-//   const getIds = await axios.get(url).then(({ data }) => data);
-//   return getIds;
-// };
 console.log(JSON.stringify("url +++++" +url))
   try {
     const response = await fetch(url);
@@ -49,13 +38,6 @@ console.log(JSON.stringify("url +++++" +url))
     }
     const json = await response.json();
     const promises = await json.kids
-      // .map((id:number) =>
-
-      //   fetch(`${Api.baseURL}item/${id}.json`).then(
-      //     response => response.json()
-      //   ).then(data => data.kids && console.log(JSON.stringify("url +++++" + data.kids)))
-
-      // )
       .map((id:number) =>
 
         fetch(`${Api.baseURL}item/${id}.json`).then(
